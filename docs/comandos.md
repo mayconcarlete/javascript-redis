69794101dd734556b44a6f43d5298d9ab3ce378d
Maycon Carlete <maycon.carlete@gmail.com>
2021-12-25 19:51:13 -0300

    feat: add redis code

---
+++ docs/comandos.md
@@ -0,0 +1,5 @@
+# docker exec -it {nome do container ou id} bash // para entrar no terminal do container
+# redis-cli para acessar o terminal do redis
+# redis-cli --scan para scanear as keys do redis
+# depois de estar acessando como cli
+# get {key name} para pegar o conteudo de uma key
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello Redis')
+const { createClient } = require('redis')
+
+;
+(async () => {
+  const client = createClient(6379, '127.0.0.1')
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.set('name', 'maykeropsq');
+  await client.set('lastname', 'carlete');
+  const value = await client.get('name');
+  console.log(value)
+})();
